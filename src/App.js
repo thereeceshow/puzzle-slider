4e8c0788411298fa2f37ef585278220df35873d6
Reece Walter <reece@reecewalter.com>
2021-04-01 20:33:45 -0400

    just need pic

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ export class App extends React.Component {
     this.state = {
       boardArray: [],
       turns: 0,
-      gameOn: false
+      gameOn: false,
+      winner: false,
     }
   }
 
@@ -28,32 +29,35 @@ export class App extends React.Component {
   }
 
   tileClick(clickedTile, index) {
-    let newArray = [...this.state.boardArray];
-    const blankTileFinder = (el) => el.isBlankTile === true;
-    let blankPosIndex = newArray.findIndex(blankTileFinder)
-    console.log(blankPosIndex)
-    let tempPos = newArray[blankPosIndex].currentPos
-    let blankRow = Math.floor(blankPosIndex / 4);
-    let blankCol = blankPosIndex % 4;
-    let clickRow = Math.floor(index / 4);
-    let clickCol = index % 4;
-    if (((clickCol === blankCol) && (Math.abs(blankRow - clickRow) === 1)) || ((clickRow === blankRow) && (Math.abs(blankCol - clickCol) === 1))) {
-      newArray[blankPosIndex].currentPos = clickedTile;
-      newArray[blankPosIndex].isBlankTile = false;
-      newArray[index].currentPos = tempPos;
-      newArray[index].isBlankTile = true;
-      this.setState(
-        {
-          boardArray: newArray,
-          turns: this.state.turns + 1
-        },
-        () => console.log(this.state)
-      );
+    if (this.state.gameOn && !this.state.winner) {
+      let newArray = [...this.state.boardArray];
+      const blankTileFinder = (el) => el.isBlankTile === true;
+      let blankPosIndex = newArray.findIndex(blankTileFinder)
+      console.log(blankPosIndex)
+      let tempPos = newArray[blankPosIndex].currentPos
+      let blankRow = Math.floor(blankPosIndex / 4);
+      let blankCol = blankPosIndex % 4;
+      let clickRow = Math.floor(index / 4);
+      let clickCol = index % 4;
+      if (((clickCol === blankCol) && (Math.abs(blankRow - clickRow) === 1)) || ((clickRow === blankRow) && (Math.abs(blankCol - clickCol) === 1))) {
+        newArray[blankPosIndex].currentPos = clickedTile;
+        newArray[blankPosIndex].isBlankTile = false;
+        newArray[index].currentPos = tempPos;
+        newArray[index].isBlankTile = true;
+        this.setState(
+          {
+            boardArray: newArray,
+            turns: this.state.turns + 1
+          },
+          () => console.log(this.state)
+        );
 
+      } this.checkWin()
     }
   }
 
   randShuffle() {
+    this.state.gameOn = true;
     let newArray = [...this.state.boardArray];
     let arrOfRandClicks = [-4, -1, 1, 4]
     for (let i = 0; i < 701; i++) {
@@ -61,47 +65,25 @@ export class App extends React.Component {
       let blankPosIndex = newArray.findIndex(blankTileFinder);
       let randClick = blankPosIndex + arrOfRandClicks[Math.floor(Math.random() * 4)]
       newArray[randClick] ? this.tileClick(newArray[randClick].currentPos, randClick) : i--
-
-      // console.log(newArray[randClick].currentPos)
-
-      // this.tileClick()
     }
-
-
+    this.setState({ 
+      gameOn: true,
+      turns: 0,
+    })
   }
 
+  checkWin() {
+    if (this.state.gameOn && this.state.turns > 1) {
+      let test = this.state.boardArray.map((el, i) => el.currentPos === i).includes(false);
+      if (!test) {
+        this.setState({
+          gameOn: false,
+          winner: true,
+        })
 
-  // newArray[blankPosIndex].currentPos = clickedTile;
-  // newArray[blankPosIndex].isBlankTile = false;
-  // newArray[index].currentPos = tempPos;
-  // newArray[index].isBlankTile = true;
-  // this.setState(
-  //   {
-  //     boardArray: newArray,
-  //     turns: this.state.turns + 1
-  //   },
-  //   () => console.log(this.state)
-  // );
-  // // if (legalMoves.includes(clickedTile)) {
-  //   console.log('Legal')
-  //   newArray[0].currentPos = clickedTile;
-  //   newArray[index].currentPos = blank;
-  //   this.setState(
-  //     {
-  //       boardArray: newArray,
-  //       turns: this.state.turns + 1 
-  //     },
-  //     () => console.log(this.state)
-  //   );
-  // } else {
-  //   console.log("Nun-Uh... Won't Do It")
-  // }
-
-  // let boardArray = this.state.boardArray;
-  // let t = boardArray[index].currentPos;
-  // boardArray[index].currentPos = boardArray[0].currentPos;
-  // boardArray[0].currentPos = t;
-  // this.setState({boardArray});
+      }
+    }
+  }
 
   render() {
 
@@ -117,17 +99,16 @@ export class App extends React.Component {
       );
     };
 
-    // const renderArray = this.state.boardArray.map(mapHelper)
-    // const renderArray = this.state.boardArray.sort((a, b) => (a.currentPos > b.currentPos) ? 1 : -1)
     const renderArray1 = this.state.boardArray.map(mapHelper)
 
     return (
       <div className="App container" >
         <h1 className="text-center mt-3 px-2">The Amazing Puzzle Slider Game</h1>
+        {this.state.winner ? <h1 className="text-center mt-3 px-2 text-success">WE HAVE A WINNER</h1> : null}
         <div className="row p-4 text-center d-flex justify-content-center">
           {renderArray1}
         </div>
-        <div className="d-flex justify-content-center"><button className="btn btn-secondary btn-large text-center" onClick={() => this.randShuffle()}>Shuffle</button></div>
+        <div className="d-flex justify-content-center"><button className="btn btn-secondary btn-large text-center" onClick={() => this.randShuffle()}>{this.state.gameOn ? 'Shuffle' : 'New Game'}</button></div>
       </div>
     )
   }
